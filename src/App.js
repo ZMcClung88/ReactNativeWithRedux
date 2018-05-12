31f6f4e3fe2f76452df0f665cf91635a34d5a100
Zac McClung <zac.mcclung370@gmail.com>
2018-05-11 23:12:39 -0600

    library list of data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
-import { Header } from './components/common';
+import { Header } from './components/common/Header';
 
 const App = () => {
   return (
--- src/components/common/index.js
+++
@@ -1,7 +0,0 @@
-// VVV import and export in one line VVV
-export * from './Button';
-export * from './Card';
-export * from './CardSection';
-export * from './Header';
-export * from './Input';
-export * from './Spinner';
